288f73e567df0a133828e776558b51d34ce72268
Linden Burack <linden.burack@gmail.com>
2023-02-23 16:00:26 -0500

    Revert "Easy Figure Generation"
    This reverts commit cf4b3b0d08ef7574538418cbd54464f551471aa4.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
 import Profile from './Profile.js'
-import useState from "react"
 
 export function App() {
   // [image, setImage] = useState()
 
   return (
     <div className="App">
-      <Profile uid="11111"></Profile>
+      <div style={{display: "flex"}}>
+        <button className="button" style={{alignContent: "left", margin: "10px"}}>
+          View my calendar
+        </button>
+      </div>
+      
+
+      <div style={{display: "flex", justifyContent: "center"}}>
+          <div style={{display: "flex", flexDirection: "row", gap:"15px"}}>
+            {/* Profile image thingy */}
+            <div style={{display: "flex", flexDirection: "column"}}>
+              <div className="placeholder-profile-pic" onClick={doThing}/>
+            </div>
+
+            <div style={{display: "flex", flexDirection: "column"}}>
+              {/* Profile info */}
+              <div className="profile-info" style={{width:"200px", textAlign:"left"}}>
+                  <h1 style={{marginTop: "0px"}}>Profile</h1>
+                  <p>Name <span className="edit">edit</span></p>
+                  <p>Username</p>
+                  <p>Email <span className="edit">edit</span></p>
+                  <p>Phone <span className="edit">edit</span></p>
+                  <button className="button">Share Calendar</button>
+              </div>
+            </div>
+          </div>
+      </div>
     </div>
   );
 }
 
-
+function doThing()
+{
+  console.log("hi")
+}
 
 export default App;
