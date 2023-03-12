c0284edb9a500195aabac7bbd2001025c17b9fd2
Linden Burack <linden.burack@gmail.com>
2023-03-12 12:15:34 -0400

    Revert "Revert "Easy Figure Generation""
    This reverts commit 288f73e567df0a133828e776558b51d34ce72268.

--- src/Profile.js
+++ src/Profile.js
@@ -1,7 +1,55 @@
-export function Profile() {
+import EditableField from "./EditableField"
+
+
+export default function Profile({uid}) {
+    // do some sort of get request to the database with the uid to retrieve information
+    const userdata = {
+        name: "Bob Johnson",
+        username: "testUsername",
+        email: "example@gmail.com",
+        phone: "123 456 7891"
+    }
+
+
     return (
         <div>
-            This is a profile page!
+            <div style={{display: "flex"}}>
+                <button className="button" style={{alignContent: "left", margin: "10px"}}>
+                View my calendar
+                </button>
+            </div>
+            
+
+            <div style={{display: "flex", justifyContent: "center"}}>
+                <div style={{display: "flex", flexDirection: "row", gap:"15px"}}>
+                    <div style={{display: "flex", flexDirection: "column"}}>
+                        {/* Profile image thingy */}
+                        <div className="placeholder-profile-pic" onClick={changepfpui}/>
+                    </div>
+
+                    <div style={{display: "flex", flexDirection: "column"}}>
+                        {/* Profile info */}
+                        <div className="profile-info" style={{width:"200px", textAlign:"left"}}>
+                            <h1 style={{marginTop: "0px"}}>Profile</h1>
+                            <EditableField value={userdata.name} onclick_fxn={doThing}/>
+                            <EditableField value={userdata.email} onclick_fxn={doThing}/>
+                            <EditableField value={userdata.phone} onclick_fxn={doThing}/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            
         </div>
     )
+}
+
+function doThing()
+{
+    console.log("clicked")
+}
+
+export function changepfpui()
+{
+  console.log("what")
 }
